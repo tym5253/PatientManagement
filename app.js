2892f029128b496da193599322c206cf1335b247
tym5253 <taha.y.m@gmail.com>
2021-04-01 20:19:35 +0530

    second commit made registration form

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 //jshint esversion:6
 const express= require("express");
 const bodyParser=require("body-parser");
+const mysql=require("mysql");
 
 const app=express();
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "tahaym@5253"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected database");
+});
+
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -12,5 +25,8 @@ app.get("/",function(req,res){
 res.render("index");
 });
 
+app.get("/registration",function(req,res){
+res.render("registration");
+});
 
 app.listen(3000,function(){console.log("server started on port 3000 succesfully");});
